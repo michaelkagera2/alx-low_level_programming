d8f5e2a6c7e4b280df2e0f15fdde0246b362e95d
Your Name <you@example.com>
2023-07-13 16:01:46 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#includes<studio.h>
+
+/**
+ * main - Entry point
+ *
+ * puts: Prints exactly \"Programming is like building a multilingual puzzle
+ * Return: Always (0) (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	retun(0);
+}	
