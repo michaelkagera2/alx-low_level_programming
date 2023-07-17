2cbfdb7c03ada81e20897606dfcced2bf3e4af9d
Your Name <you@example.com>
2023-07-17 14:54:46 -0700

    56 letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char l[56] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYS";
+	int x = 0;
+
+	do {
+	putchar(l[x]);
+	x++;
+	} while (x < 56);
+	putchar ('\n');
+	return (0);
+}
