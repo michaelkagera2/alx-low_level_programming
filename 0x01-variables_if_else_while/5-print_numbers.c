c50b36cfde47ae6935ea64cf8d6bb1e4bcafbeb7
Your Name <you@example.com>
2023-07-17 15:34:43 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x = 10; x++)
+	{
+		printf("%d", x);
+	}
+	putchar ('\n');
+	return (0);
+
+}
