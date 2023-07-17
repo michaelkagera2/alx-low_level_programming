4f97fd389ab57bb8161a3ece4304a9e6053ff096
Your Name <you@example.com>
2023-07-17 15:23:19 -0700

    skip q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char l[26] = "abcdefghijklmnopqrstuvwxyz";
+	int x = 0;
+
+	do {
+		if (x != 16  && x != 4)
+			{
+				putchar(l[x]);
+			}
+			x++;
+	} while (x < 26);
+	putchar ('\n');
+	return (0);
+}
