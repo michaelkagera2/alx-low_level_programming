8aea024f05b9390bcf6fab366b8368f80e8f2411
Your Name <you@example.com>
2023-07-19 13:20:54 -0700

    alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints alphabet 10 times
+ *
+ * Description: Calls print alphabet 10 times
+*/
+
+void print_alphabet_x10(void)
+{
+	int x
+
+	do {
+		print_alphabet();
+		return (0);
+		x++;
+	} while (x <= 0);
+
+	putchar('\n);
+}
