9b968a86c90b2379771f87c57a1f947a316eb4e5
Your Name <you@example.com>
2023-08-01 08:16:37 -0700

    correction

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,25 +11,29 @@
  */
 void print_remaining_days(int month, int day, int year)
 {
+	int days_in_feb;
+
 	if (year % 4 == 0 && (year % 400 == 0 || year % 100 != 0))
 	{
-		if (month >= 2 && day >= 60)
-		{
-			day++;
-		}
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
+		days_in_feb = 29;
 	}
 	else
 	{
-		if (month == 2 && day == 60)
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-		}
-		else
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
-		}
+		day_in_feb = 28;
+	}
+
+	if (month >= 2 && day > (31 + days_in_feb))
+	{
+		day--;
+	}
+
+	if (month == 2 && day == 60)
+	{
+		printf("Invalid date: %02d/%02d/%04d\n", month, day - 1, year);
+	}
+	else
+	{
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 365 + (days_in_feb == 29) - day);
 	}
 }
