36272e30d2820580eaaf04691a6a3847c236bd00
Your Name <you@example.com>
2023-08-01 13:04:59 -0700

    new

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,36 +1,54 @@
 #include "main.h"
 
 /**
- * rev_string - reverse array
- * @n: string to reverse
+ * infinite_add - Adds two positive numbers represented as strings
+ * @n1: Pointer to the first number string
+ * @n2: Pointer to the second number string
+ * @r: Pointer to the buffer that stores the result
+ * @size_r: The size of the buffer
  *
- * Return: void
+ * Return: Pointer to the result or 0 if the result cannot be stored in r
  */
-void rev_string(char *n)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0;
-	int j = 0;
+	int len_n1 = 0, len_n2 = 0, carry = 0, sum;
+	int i, j, k;
 	char temp;
 
-	while (*(n + i) != '\0')
-	{
-		i++;
-	}
-	i--;
+	while (n1[len_n1] != '\0')
+		len_n1++;
+	while (n2[len_n2] != '\0')
+		len_n2++;
 
-	for (j = 0; j < i; j++, i--)
+	if (len_n1 >= size_r || len_n2 >= size_r)
+		return (0);
+
+	i = len_n1 - 1, j = len_n2 - 1, k = 0;
+
+	while (i >= 0 || j >= 0 || carry)
 	{
-		temp = *(n + j);
-		*(n + j) = *(n + i);
-		*(n + i) = temp;
+		sum = carry;
+		if (i >= 0)
+			sum += n1[i] - '0';
+		if (j >= 0)
+			sum += n2[j] - '0';
+
+		carry = sum / 10;
+		if (k < size_r - 1)
+			r[k] = (sum % 10) + '0';
+		else
+			return (0);
+		i--;
+		j--;
+		k++;
 	}
+	r[k] = '\0';
+
+	for (i = 0, j = k - 1; i < j; i++, j--)
+	{
+		temp = r[i];
+		r[i] = r[j];
+		r[j] = temp;
+	}
+	return (r);
 }
-
-/**
- * infinite_add - add 2 numbers together
- * @n1: text representation of 1st number to add
- * @n2: text representation of 2nd number to add
- * @r: pointer to buffer
- * @size_r: buffer size
- * Return: pointer to calling function
- */
